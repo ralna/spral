28d440004f529d9bb125929e4fca8f4f56fd817e
Jonathan Hogg <jonathan.hogg@stfc.ac.uk>
2014-04-04 15:12:29 +0000

    Make TOC play much nicer
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/spral/trunk@35 33810a50-c353-450f-be6c-41f7241c649d

--- doc/createweb.sh
+++ doc/createweb.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-htlatex spral.tex "spralweb.cfg, charset=utf-8, frames" " -cunihtf -utf8" "-d/numerical/www/numerical-www/spral/doc/"
+htlatex spral.tex "spralweb.cfg, 2, charset=utf-8, frames" " -cunihtf -utf8" "-d/numerical/www/numerical-www/spral/doc/"
 rm *.html *.4ct *.4tc *.aux *.css *.dvi *.idv *.lg *.log *.tmp *.xref
