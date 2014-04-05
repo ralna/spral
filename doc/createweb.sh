31794d20e81414246df40eb74f14832594b9dc44
Jonathan Hogg <jonathan.hogg@stfc.ac.uk>
2014-04-05 21:03:25 +0000

    Pull custom CSS out of .cfg and into .css for ease of handling
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/spral/trunk@42 33810a50-c353-450f-be6c-41f7241c649d

--- doc/createweb.sh
+++ doc/createweb.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 htlatex spral.tex "spralweb.cfg, charset=utf-8" " -cunihtf -utf8" "-d/numerical/www/numerical-www/spral/doc/"
-rm *.html *.4ct *.4tc *.aux *.css *.dvi *.idv *.lg *.log *.tmp *.xref
+rm *.html *.4ct *.4tc *.aux spral.css *.dvi *.idv *.lg *.log *.tmp *.xref
