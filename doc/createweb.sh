a5bc648dd6f102283bf7b8d3c05aebd0a7f63553
Jonathan Hogg <jonathan.hogg@stfc.ac.uk>
2014-04-06 09:24:45 +0000

    Copy everything required as symlinks don't cut it
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/spral/trunk@43 33810a50-c353-450f-be6c-41f7241c649d

--- doc/createweb.sh
+++ doc/createweb.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-htlatex spral.tex "spralweb.cfg, charset=utf-8" " -cunihtf -utf8" "-d/numerical/www/numerical-www/spral/doc/"
+DESTDIR=/numerical/www/numerical-www/spral/doc/
+
+htlatex spral.tex "spralweb.cfg, charset=utf-8" " -cunihtf -utf8" "-d$DESTDIR"
 rm *.html *.4ct *.4tc *.aux spral.css *.dvi *.idv *.lg *.log *.tmp *.xref
+cp spral.custom.css spral.js $DESTDIR
+cp stfc.png epsrc.png arrow_down16.png arrow_right16.png $DESTDIR
+ln -s $DESTDIR/spral.html $DESTDIR/index.html
