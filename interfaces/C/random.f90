1d0b985cafb0a04bf51ed83b086623b9cdcbd97c
Jonathan Hogg <jonathan.hogg@stfc.ac.uk>
2014-04-08 10:27:51 +0000

    Fix and expand RANDOM C interface
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/spral/trunk@66 33810a50-c353-450f-be6c-41f7241c649d

--- interfaces/C/random.f90
+++ interfaces/C/random.f90
@@ -1,10 +1,60 @@
 real(C_DOUBLE) function spral_random_real(cstate, cpositive) bind(C)
+   use iso_c_binding
+   use spral_random
+   implicit none
+
    integer(C_INT), intent(inout) :: cstate
    logical(C_BOOL), value :: cpositive
 
    type(random_state) :: fstate
+   logical :: fpositive
 
+   ! Initialize state
    call random_set_seed(fstate, cstate)
-   spral_random_real = random_real(fstate, positive=cpositive)
+
+   ! Call Fortran routine
+   fpositive = cpositive
+   spral_random_real = random_real(fstate, positive=fpositive)
+
+   ! Recover state
    cstate = random_get_seed(fstate)
 end function spral_random_real
+
+integer(C_INT) function spral_random_integer(cstate, n) bind(C)
+   use iso_c_binding
+   use spral_random
+   implicit none
+
+   integer(C_INT), intent(inout) :: cstate
+   integer(C_INT), value :: n
+
+   type(random_state) :: fstate
+
+   ! Initialize state
+   call random_set_seed(fstate, cstate)
+
+   ! Call Fortran routine
+   spral_random_integer = random_integer(fstate, n)
+
+   ! Recover state
+   cstate = random_get_seed(fstate)
+end function spral_random_integer
+
+logical(C_BOOL) function spral_random_logical(cstate) bind(C)
+   use iso_c_binding
+   use spral_random
+   implicit none
+
+   integer(C_INT), intent(inout) :: cstate
+
+   type(random_state) :: fstate
+
+   ! Initialize state
+   call random_set_seed(fstate, cstate)
+
+   ! Call Fortran routine
+   spral_random_logical = random_logical(fstate)
+
+   ! Recover state
+   cstate = random_get_seed(fstate)
+end function spral_random_logical
