0447444c96185a7d7bc77fd9f88a6c3bdb2ebcb2
Jonathan Hogg <jonathan.hogg@stfc.ac.uk>
2014-04-09 08:15:39 +0000

    Rearrange doc into Fortran and C directories
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/spral/trunk@81 33810a50-c353-450f-be6c-41f7241c649d

---
+++ doc/Fortran/createpdf.sh
@@ -0,0 +1,1 @@
+../createpdf.sh
--- doc/createpdf.sh
+++ doc/createpdf.sh
@@ -2,14 +2,23 @@
 
 BASENAME=`echo "$1" | sed 's/\.tex//g'`
 WRAPPERFILE=${BASENAME}_wrapper
+WORKDIR=work.$$
+
+mkdir $WORKDIR
+pushd $WORKDIR
 
 echo "\\documentclass{spral}
 \\begin{document}
-\\include{$BASENAME}
+\\input{../$BASENAME}
 \\end{document}
 " > ${WRAPPERFILE}.tex
 
+ln -s ../../spral.cls
+ln -s ../../stfc.png
+ln -s ../../epsrc.png
+ln -s ../../../examples
 pdflatex ${WRAPPERFILE}.tex
 pdflatex ${WRAPPERFILE}.tex
-mv ${WRAPPERFILE}.pdf ${BASENAME}.pdf
-rm ${WRAPPERFILE}.*
+mv ${WRAPPERFILE}.pdf ../${BASENAME}.pdf
+popd
+rm -r $WORKDIR
