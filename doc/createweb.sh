4044f75e56b220116275e68a2ba5a6ddd4d33488
Jonathan Hogg <jonathan.hogg@stfc.ac.uk>
2014-04-09 14:42:15 +0000

    Adapt web to generate differnitiated C/Fortran directories
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/spral/trunk@94 33810a50-c353-450f-be6c-41f7241c649d

--- doc/createweb.sh
+++ doc/createweb.sh
@@ -1,9 +1,37 @@
 #!/bin/bash
 
-DESTDIR=/numerical/www/numerical-www/spral/doc/
+DOCDIR=/numerical/www/numerical-www/spral/doc
 
+# Fortran
+DESTDIR=$DOCDIR/Fortran/
+WORKDIR=workF.$$
+mkdir $WORKDIR
+pushd $WORKDIR
+ln -s ../Fortran/*.tex .
+ln -s ../spral.tex ../install.tex ../spralweb.cls ../spralweb.cfg .
+ln -s ../../examples
+ln -s ../../LICENCE
 htlatex spral.tex "spralweb.cfg, charset=utf-8" " -cunihtf -utf8" "-d$DESTDIR"
-rm *.html *.4ct *.4tc *.aux spral.css *.dvi *.idv *.lg *.log *.tmp *.xref
+popd
+ln -s $DESTDIR/spral.html $DESTDIR/index.html
+rm -r $WORKDIR
+
+# C
+DESTDIR=$DOCDIR/C/
+WORKDIR=workC.$$
+mkdir $WORKDIR
+pushd $WORKDIR
+ln -s ../C/*.tex .
+ln -s ../spral.tex ../install.tex ../spralweb.cls ../spralweb.cfg .
+ln -s ../../examples
+ln -s ../../LICENCE
+htlatex spral.tex "spralweb.cfg, charset=utf-8" " -cunihtf -utf8" "-d$DESTDIR"
+popd
 cp spral.custom.css spral.js $DESTDIR
 cp stfc.png epsrc.png arrow_down16.png arrow_right16.png $DESTDIR
 ln -s $DESTDIR/spral.html $DESTDIR/index.html
+rm -r $WORKDIR
+
+# Shared
+cp spral.custom.css spral.js $DOCDIR
+cp stfc.png epsrc.png arrow_down16.png arrow_right16.png $DOCDIR
