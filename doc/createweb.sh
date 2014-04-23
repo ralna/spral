35c013ad931b2a3b08caa7ffb7b42d112ef6039f
Jonathan Hogg <jonathan.hogg@stfc.ac.uk>
2014-04-23 15:41:36 +0000

    Fix links to be not absolute
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/spral/trunk@114 33810a50-c353-450f-be6c-41f7241c649d

--- doc/createweb.sh
+++ doc/createweb.sh
@@ -12,8 +12,10 @@ ln -s ../spral.tex ../install.tex ../spralweb.cls ../spralweb.cfg .
 ln -s ../../examples
 ln -s ../../LICENCE
 htlatex spral.tex "spralweb.cfg, charset=utf-8" " -cunihtf -utf8" "-d$DESTDIR"
-popd
-ln -s $DESTDIR/spral.html $DESTDIR/index.html
+popd # WORKDIR
+pushd $DESTDIR
+ln -s spral.html index.html
+popd # DESTDIR
 rm -r $WORKDIR
 
 # C
@@ -26,10 +28,12 @@ ln -s ../spral.tex ../install.tex ../spralweb.cls ../spralweb.cfg .
 ln -s ../../examples
 ln -s ../../LICENCE
 htlatex spral.tex "spralweb.cfg, charset=utf-8" " -cunihtf -utf8" "-d$DESTDIR"
-popd
+popd # WORKDIR
 cp spral.custom.css spral.js $DESTDIR
 cp stfc.png epsrc.png arrow_down16.png arrow_right16.png $DESTDIR
-ln -s $DESTDIR/spral.html $DESTDIR/index.html
+pushd $DESTDIR
+ln -s spral.html index.html
+popd # DESTDIR
 rm -r $WORKDIR
 
 # Shared
