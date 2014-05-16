459dfbaa3aeb3f89a275706a0b69af815f26c690
Jonathan Hogg <jonathan.hogg@stfc.ac.uk>
2014-05-16 08:44:17 +0000

    Add syrk stuff
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/spral/branches/paperMay2014@123 33810a50-c353-450f-be6c-41f7241c649d

--- blas_bench.cu
+++ blas_bench.cu
@@ -52,6 +52,50 @@ int bench_nostream(int maxdimn, double *gpu_work) {
    return 0;
 }
 
+int bench_nostream_syrk(int maxdimn, double *gpu_work) {
+   char buffer[10000];
+   cublasHandle_t cublas;
+   cublasCreate(&cublas);
+
+   struct timespec start, end;
+   cudaDeviceSynchronize();
+   clock_gettime(CLOCK_REALTIME, &start);
+
+   long flops = 0;
+   while(!feof(stdin)) {
+      fgets(buffer, sizeof buffer, stdin);
+      if(buffer[0] == '#') continue; // Skip comments
+      // Read data
+      int node, lev, blkm, blkn;
+      sscanf(buffer, "%d %d %d %d", &node, &lev, &blkm, &blkn);
+      long m = blkm-blkn;
+      if(2*m*blkn+m*m > maxdimn) {
+         printf("Can't cope with node %d being %d x %d\n", node, blkm, blkn);
+         return 1;
+      }
+      // Launch BLAS
+      double alpha = -1.0;
+      double beta = 1.0;
+      double *a = gpu_work;
+      double *b = a + m*blkn;
+      double *c = b + m*blkn;
+      if(blkm-blkn != 0 && blkn != 0)
+         CublasSafeCall(
+            cublasDsyrk(cublas, CUBLAS_FILL_MODE_LOWER, CUBLAS_OP_N,
+               blkm-blkn, blkn, &alpha, a, blkm-blkn, &beta, c, blkm-blkn)
+            );
+      flops += 2*(m*(m+1)*blkn)/2; // only do half matrix
+   }
+   cudaDeviceSynchronize();
+   clock_gettime(CLOCK_REALTIME, &end);
+   printf("nostream: Did %e flops\n", (float) flops);
+   printf("nostream: Took %f seconds\n", tdiff(start, end));
+   printf("nostream: Rate %f Gflop/s\n", flops/(1e9*tdiff(start,end)));
+
+   cublasDestroy(cublas);
+   return 0;
+}
+
 int bench_stream(int maxdimn, double *gpu_work) {
    char buffer[10000];
    cudaStream_t stream[16];
@@ -112,6 +156,65 @@ int bench_stream(int maxdimn, double *gpu_work) {
    return 0;
 }
 
+int bench_stream_syrk(int maxdimn, double *gpu_work) {
+   char buffer[10000];
+   cudaStream_t stream[16];
+   for(int i=0; i<16; i++) CudaSafeCall( cudaStreamCreate(&stream[i]) );
+   cublasHandle_t cublas[16];
+   for(int i=0; i<16; i++) CublasSafeCall( cublasCreate(&cublas[i]) );
+   for(int i=0; i<16; i++) CublasSafeCall( cublasSetStream(cublas[i], stream[i]) );
+
+   struct timespec start, end;
+   cudaDeviceSynchronize();
+   clock_gettime(CLOCK_REALTIME, &start);
+
+   long flops = 0;
+   int lastlev = 1;
+   int s=0;
+   while(!feof(stdin)) {
+      fgets(buffer, sizeof buffer, stdin);
+      if(buffer[0] == '#') continue; // Skip comments
+      // Read data
+      int node, lev, blkm, blkn;
+      sscanf(buffer, "%d %d %d %d", &node, &lev, &blkm, &blkn);
+      long m = blkm-blkn;
+      if(2*m*blkn+m*m > maxdimn) {
+         printf("Can't cope with node %d being %d x %d\n", node, blkm, blkn);
+         return 1;
+      }
+      // Check for level change
+      if(lev != lastlev) {
+         cudaDeviceSynchronize();
+         lastlev = lev;
+      }
+      // Launch BLAS
+      double alpha = -1.0;
+      double beta = 1.0;
+      double *a = gpu_work;
+      double *b = a + m*blkn;
+      double *c = b + m*blkn;
+      //printf("running on stream %d: %d %d %d\n", s, blkm-blkn, blkm-blkn, blkn);
+      if(blkm-blkn != 0 && blkn != 0)
+         CublasSafeCall(
+            cublasDsyrk(cublas[s], CUBLAS_FILL_MODE_LOWER, CUBLAS_OP_N,
+               blkm-blkn, blkn, &alpha, a, blkm-blkn, &beta, c, blkm-blkn)
+            );
+      flops += 2*(m*(m+1)*blkn)/2; // only do half matrix
+      if(++s >= 16) s=0;
+   }
+   CudaSafeCall( cudaDeviceSynchronize() );
+   clock_gettime(CLOCK_REALTIME, &end);
+   printf("stream: Did %e flops\n", (float) flops);
+   printf("stream: Took %f seconds\n", tdiff(start, end));
+   printf("stream: Rate %f Gflop/s\n", flops/(1e9*tdiff(start,end)));
+
+   for(int i=0; i<16; i++) {
+      CublasSafeCall( cublasDestroy(cublas[i]) );
+      CudaSafeCall( cudaStreamDestroy(stream[i]) );
+   }
+   return 0;
+}
+
 int main(void) {
 
    const int maxdimn = 3*5000*5000;
@@ -130,7 +233,9 @@ int main(void) {
    cudaMemcpy(gpu_work, work, maxdimn*sizeof(double), cudaMemcpyHostToDevice);
 
    //if(!bench_nostream(maxdimn, gpu_work)) { return 1; }
-   if(!bench_stream(maxdimn, gpu_work)) { return 1; }
+   //if(!bench_stream(maxdimn, gpu_work)) { return 1; }
+   if(!bench_nostream_syrk(maxdimn, gpu_work)) { return 1; }
+   //if(!bench_stream_syrk(maxdimn, gpu_work)) { return 1; }
 
    cudaFree(gpu_work);
    free(work);
