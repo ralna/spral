0cf24f8ad1ecc280769cb9485a162deb314aef2d
Jonathan Hogg <jonathan.hogg@stfc.ac.uk>
2016-06-27 11:15:10 +0000

    Fix up inform_nogpu
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/spral/branches/cpu_ssids@682 33810a50-c353-450f-be6c-41f7241c649d

--- src/ssids/gpu/inform_nogpu.f90
+++ src/ssids/gpu/inform_nogpu.f90
@@ -1,5 +1,5 @@
 ! Version of module providing minimum functionality in case of no GPU support
-module spral_ssids_inform_gpu
+module spral_ssids_gpu_inform
    use spral_ssids_inform, only : ssids_inform_base
    implicit none
 
@@ -10,4 +10,4 @@ module spral_ssids_inform_gpu
       integer :: cuda_error = 0 ! cuda error value
       integer :: cublas_error = 0 ! cublas error value
    end type ssids_inform_gpu
-end module spral_ssids_inform_gpu
+end module spral_ssids_gpu_inform
