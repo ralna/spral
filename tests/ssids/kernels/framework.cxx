3f840058185db898077bed73719dfc27754cab0f
Jonathan Hogg <jonathan.hogg@stfc.ac.uk>
2016-07-13 14:57:35 +0000

    Propogate NaNs so we get fails
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/spral/branches/cpu_ssids@780 33810a50-c353-450f-be6c-41f7241c649d

--- tests/ssids/kernels/framework.cxx
+++ tests/ssids/kernels/framework.cxx
@@ -103,11 +103,13 @@ double backward_error(int n, double const* a, int lda, double const* rhs, int nr
       for(int i=0; i<n; ++i) {
          rhsnorm = std::max(rhsnorm, fabs(rhs[i]));
          residnorm = std::max(residnorm, fabs(resid[i]));
+         if(isnan(resid[i])) residnorm = resid[i]; 
          solnnorm = std::max(solnnorm, fabs(r*ldsoln+soln[i]));
       }
 
       //printf("%e / %e %e %e\n", residnorm, anorm, solnnorm, rhsnorm);
       worstbwderr = std::max(worstbwderr, residnorm/(anorm*solnnorm + rhsnorm));
+      if(isnan(residnorm)) worstbwderr = residnorm;
    }
 
    /* Cleanup */
