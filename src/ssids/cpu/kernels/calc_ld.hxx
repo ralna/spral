509d67b8144586f08c3936aa81cd38b4eaf1b41a
Jonathan Hogg <jonathan.hogg@stfc.ac.uk>
2016-08-02 11:01:11 +0000

    Explicitly vectorize for efficiency
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/spral/branches/cpu_ssids@937 33810a50-c353-450f-be6c-41f7241c649d

--- src/ssids/cpu/kernels/calc_ld.hxx
+++ src/ssids/cpu/kernels/calc_ld.hxx
@@ -11,22 +11,66 @@
 #pragma once
 
 #include <cmath>
+#include <stdexcept>
 
 #include "common.hxx"
+#include "SimdVec.hxx"
 
 namespace spral { namespace ssids { namespace cpu {
 
-/** Calculates LD from L and D */
+/** Calculates LD from L and D.
+ *
+ * We assume that both l and ld are 32-bytes aligned, and ldl and ldld are
+ * multiples of 32 bytes, so we can use AVX.
+ */
 template <enum operation op, typename T>
-void calcLD(int m, int n, const T *l, int ldl, const T *d, T *ld, int ldld) {
+void calcLD(int m, int n, T const* l, int ldl, T const* d, T* ld, int ldld) {
+   typedef SimdVec<T> SimdVecT;
+
    for(int col=0; col<n; ) {
       if(col+1==n || std::isfinite(d[2*col+2])) {
          // 1x1 pivot
          T d11 = d[2*col];
          if(d11 != 0.0) d11 = 1/d11; // Zero pivots just cause zeroes
-         for(int row=0; row<m; row++)
-            ld[col*ldld+row] = d11 * ((op==OP_N) ? l[col*ldl+row]
-                                                 : l[row*ldl+col]);
+         if(op==OP_N) {
+            int const vlen = SimdVecT::vector_length;
+            int const unroll = 4;
+            int nvec = m / vlen;
+            SimdVecT d11v(d11);
+            if(nvec < unroll) {
+               for(int row=0; row<nvec*vlen; row+=vlen) {
+                  SimdVecT lv = SimdVecT::load_aligned(&l[col*ldl+row]);
+                  lv = lv * d11;
+                  lv.store_aligned(&ld[col*ldld+row]);
+               }
+            } else {
+               int nunroll = nvec / unroll;
+               for(int row=0; row<nunroll*unroll*vlen; row+=unroll*vlen) {
+                  SimdVecT lv0 = SimdVecT::load_aligned(&l[col*ldl+row+0*vlen]);
+                  SimdVecT lv1 = SimdVecT::load_aligned(&l[col*ldl+row+1*vlen]);
+                  SimdVecT lv2 = SimdVecT::load_aligned(&l[col*ldl+row+2*vlen]);
+                  SimdVecT lv3 = SimdVecT::load_aligned(&l[col*ldl+row+3*vlen]);
+                  lv0 *= d11;
+                  lv1 *= d11;
+                  lv2 *= d11;
+                  lv3 *= d11;
+                  lv0.store_aligned(&ld[col*ldld+row+0*vlen]);
+                  lv1.store_aligned(&ld[col*ldld+row+1*vlen]);
+                  lv2.store_aligned(&ld[col*ldld+row+2*vlen]);
+                  lv3.store_aligned(&ld[col*ldld+row+3*vlen]);
+               }
+               for(int row=nunroll*unroll*vlen; row<nvec*vlen; row+=vlen) {
+                  SimdVecT lv = SimdVecT::load_aligned(&l[col*ldl+row]);
+                  lv = lv * d11;
+                  lv.store_aligned(&ld[col*ldld+row]);
+               }
+            }
+            for(int row=nvec*vlen; row<m; row++)
+               ld[col*ldld+row] = d11 * l[col*ldl+row];
+         } else { /* op==OP_T */
+            for(int row=0; row<m; row++)
+               ld[col*ldld+row] = d11 * l[row*ldl+col];
+         }
          col++;
       } else {
          // 2x2 pivot
@@ -38,8 +82,14 @@ void calcLD(int m, int n, const T *l, int ldl, const T *d, T *ld, int ldld) {
          d21 = d21/det;
          d22 = d22/det;
          for(int row=0; row<m; row++) {
-            T a1 = (op==OP_N) ? l[col*ldl+row]     : l[row*ldl+col];
-            T a2 = (op==OP_N) ? l[(col+1)*ldl+row] : l[row*ldl+(col+1)];
+            T a1, a2;
+            if(op==OP_N) {
+               a1 = l[col*ldl+row];
+               a2 = l[(col+1)*ldl+row];
+            } else { /* op==OP_T */
+               a1 = l[row*ldl+col];
+               a2 = l[row*ldl+(col+1)];
+            }
             ld[col*ldld+row]     =  d22*a1 - d21*a2;
             ld[(col+1)*ldld+row] = -d21*a1 + d11*a2;
          }
