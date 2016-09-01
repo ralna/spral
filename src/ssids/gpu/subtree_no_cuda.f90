e799b1211cfb6a756466ccc57b04b1fb9712a634
Jonathan Hogg <jonathan.hogg@stfc.ac.uk>
2016-09-01 08:06:56 +0000

    Add some dummy operations to stop warnings on compile
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/spral/branches/cpu_ssids@1103 33810a50-c353-450f-be6c-41f7241c649d

--- src/ssids/gpu/subtree_no_cuda.f90
+++ src/ssids/gpu/subtree_no_cuda.f90
@@ -12,12 +12,14 @@ module spral_ssids_gpu_subtree
    public :: gpu_numeric_subtree, gpu_free_contrib
 
    type, extends(symbolic_subtree_base) :: gpu_symbolic_subtree
+      integer(long) :: dummy
    contains
       procedure :: factor
       final :: symbolic_final
    end type gpu_symbolic_subtree
 
    type, extends(numeric_subtree_base) :: gpu_numeric_subtree
+      real(wp) :: dummy !< Just so we can perform dummy ops to prevent warnings
    contains
       procedure :: get_contrib
       procedure :: solve_fwd
@@ -50,10 +52,18 @@ function construct_gpu_symbolic_subtree(device, n, sa, en, sptr, sparent, &
    print *, "construct_gpu_symbolic_subtree() called without GPU support."
    print *, "This should never happen."
    stop -1
+
+   ! Dummy operations to prevent warnings
+   nullify(this)
+   this%dummy = device+n+sa+en+sptr(1)+sparent(1)+rptr(1)+rlist(1)+nptr(1)+&
+      nlist(1,1)+options%cpu_small_subtree_threshold
 end function construct_gpu_symbolic_subtree
 
 subroutine symbolic_final(this)
    type(gpu_symbolic_subtree) :: this
+
+   ! Dummy operation to prevent warnings
+   this%dummy = 0
 end subroutine symbolic_final
 
 function factor(this, posdef, aval, child_contrib, options, inform, scaling)
@@ -65,19 +75,38 @@ function factor(this, posdef, aval, child_contrib, options, inform, scaling)
    type(ssids_options), intent(in) :: options
    type(ssids_inform), intent(inout) :: inform
    real(wp), dimension(*), target, optional, intent(in) :: scaling
+
+   type(gpu_numeric_subtree), pointer :: subtree
+
+   ! Dummy operations to prevent warnings
+   nullify(subtree)
+   factor => subtree
+   if(posdef) &
+      subtree%dummy = real(this%dummy,wp)+aval(1)+child_contrib(1)%val(1)+&
+         options%gpu_perf_coeff
+   if(present(scaling)) subtree%dummy = subtree%dummy * scaling(1)
+   inform%flag = SSIDS_ERROR_UNKNOWN
 end function factor
 
 subroutine numeric_final(this)
    type(gpu_numeric_subtree) :: this
+
+   this%dummy = 0
 end subroutine numeric_final
 
 function get_contrib(this)
    type(contrib_type) :: get_contrib
    class(gpu_numeric_subtree), intent(in) :: this
+
+   ! Dummy operation to prevent warnings
+   get_contrib%n = int(this%dummy)
 end function get_contrib
 
 subroutine gpu_free_contrib(contrib)
    type(contrib_type), intent(inout) :: contrib
+
+   ! Dummy operation to prevent warnings
+   contrib%n = 0
 end subroutine gpu_free_contrib
 
 subroutine solve_fwd(this, nrhs, x, ldx, inform)
@@ -86,6 +115,10 @@ subroutine solve_fwd(this, nrhs, x, ldx, inform)
    real(wp), dimension(*), intent(inout) :: x
    integer, intent(in) :: ldx
    type(ssids_inform), intent(inout) :: inform
+
+   ! Dummy operations to prevent warnings
+   x(nrhs+1*ldx) = this%dummy
+   inform%flag = SSIDS_ERROR_UNKNOWN
 end subroutine solve_fwd
 
 subroutine solve_diag(this, nrhs, x, ldx, inform)
@@ -94,6 +127,10 @@ subroutine solve_diag(this, nrhs, x, ldx, inform)
    real(wp), dimension(*), intent(inout) :: x
    integer, intent(in) :: ldx
    type(ssids_inform), intent(inout) :: inform
+
+   ! Dummy operations to prevent warnings
+   x(nrhs+1*ldx) = this%dummy
+   inform%flag = SSIDS_ERROR_UNKNOWN
 end subroutine solve_diag
 
 subroutine solve_diag_bwd(this, nrhs, x, ldx, inform)
@@ -102,6 +139,10 @@ subroutine solve_diag_bwd(this, nrhs, x, ldx, inform)
    real(wp), dimension(*), intent(inout) :: x
    integer, intent(in) :: ldx
    type(ssids_inform), intent(inout) :: inform
+
+   ! Dummy operations to prevent warnings
+   x(nrhs+1*ldx) = this%dummy
+   inform%flag = SSIDS_ERROR_UNKNOWN
 end subroutine solve_diag_bwd
 
 subroutine solve_bwd(this, nrhs, x, ldx, inform)
@@ -110,22 +151,36 @@ subroutine solve_bwd(this, nrhs, x, ldx, inform)
    real(wp), dimension(*), intent(inout) :: x
    integer, intent(in) :: ldx
    type(ssids_inform), intent(inout) :: inform
+
+   ! Dummy operations to prevent warnings
+   x(nrhs+1*ldx) = this%dummy
+   inform%flag = SSIDS_ERROR_UNKNOWN
 end subroutine solve_bwd
 
 subroutine enquire_posdef(this, d)
    class(gpu_numeric_subtree), target, intent(in) :: this
    real(wp), dimension(*), target, intent(out) :: d
+
+   ! Dummy operation to prevent warnings
+   d(1) = this%dummy
 end subroutine enquire_posdef
 
 subroutine enquire_indef(this, piv_order, d)
    class(gpu_numeric_subtree), target, intent(in) :: this
    integer, dimension(*), target, optional, intent(out) :: piv_order
    real(wp), dimension(2,*), target, optional, intent(out) :: d
+
+   ! Dummy operation to prevent warnings
+   if(present(d)) d(1,1) = this%dummy
+   if(present(piv_order)) piv_order(1) = 1
 end subroutine enquire_indef
 
 subroutine alter(this, d)
    class(gpu_numeric_subtree), target, intent(inout) :: this
    real(wp), dimension(2,*), intent(in) :: d
+
+   ! Dummy operation to prevent warnings
+   this%dummy = d(1,1)
 end subroutine alter
 
 end module spral_ssids_gpu_subtree
