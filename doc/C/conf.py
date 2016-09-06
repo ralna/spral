c523497b361cefb2d6a2d07829a2b618dc24af52
Jonathan Hogg <jhogg41@gmail.com>
2016-09-06 18:42:48 +0100

    Convert C/random to rst from tex

--- doc/C/conf.py
+++ doc/C/conf.py
@@ -81,7 +81,7 @@ exclude_patterns = ['_build']
 
 # If true, the current module name will be prepended to all description
 # unit titles (such as .. function::).
-#add_module_names = True
+add_module_names = False
 
 # If true, sectionauthor and moduleauthor directives will be shown in the
 # output. They are ignored by default.
@@ -120,17 +120,17 @@ html_theme = 'sphinx_rtd_theme'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = "../STFC-Logo.png"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = None
+html_favicon = "../favicon.ico"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['../static']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
@@ -259,3 +259,6 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+
+def setup(app):
+    app.add_stylesheet('css/custom.css')
---
+++ doc/C/index.rst
@@ -0,0 +1,37 @@
+.. spral documentation master file, created by
+   sphinx-quickstart on Mon Feb 15 15:20:34 2016.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+The Sparse Parallel Robust Algorithms Library
+=============================================
+
+Data Formats
+
+..
+   .. toctree::
+      :maxdepth: 1
+      csc_format
+      coord_format
+
+Packages:
+
+.. toctree::
+   :maxdepth: 1
+
+   random
+
+..
+   lsmr
+   scaling
+   ssids
+   ssmfe
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
