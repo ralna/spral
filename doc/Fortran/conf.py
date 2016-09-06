6b58e38a7b5025ee477f76bdf04a46aa283ccae8
Jonathan Hogg <jhogg41@gmail.com>
2016-09-06 12:38:04 +0100

    Add some css hacks for better presentation.

--- doc/Fortran/conf.py
+++ doc/Fortran/conf.py
@@ -130,7 +130,7 @@ html_favicon = "../favicon.ico"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['../static']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
@@ -259,3 +259,6 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+
+def setup(app):
+    app.add_stylesheet('css/custom.css')
