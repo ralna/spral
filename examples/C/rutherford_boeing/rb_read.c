27b2632b65a550cb9b811388c6aada4d43002d7f
Jonathan Hogg <jhogg41@gmail.com>
2016-09-12 17:37:27 +0100

    Add spral_rb_read() example for C

---
+++ examples/C/rutherford_boeing/rb_read.c
@@ -0,0 +1,29 @@
+/* examples/C/rutherford_boeing/rb_read.c
+ * Example code for SPRAL_RUTHERFORD_BOEING */
+#include <stdio.h>
+#include "spral.h"
+
+int main(void) {
+   /* Matrix data */
+   void *handle;
+   char title[73]; // Maximum length 72 character + '\0'
+   int matrix_type, m, n;
+   long *ptr;
+   int *row;
+   double *val;
+
+   /* Initalise options */
+   struct spral_rb_read_options options;
+   spral_rb_default_read_options(&options);
+
+   /* Read matrix */
+   spral_rb_read("matrix.rb", &handle, &matrix_type, &m, &n, &ptr, &row, &val,
+         &options, title, NULL, NULL);
+
+   /* Print matrix */
+   printf("Matrix '%s'\n", title);
+   spral_print_matrix(-1, matrix_type, m, n, ptr, row, val, 0);
+
+   /* Free handle */
+   spral_rb_free_handle(&handle);
+}
