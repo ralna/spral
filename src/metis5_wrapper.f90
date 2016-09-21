28efa28e806cf2b300c9e0d24961fd477c6596dc
Jonathan Hogg <jhogg41@gmail.com>
2016-09-21 12:17:21 +0100

    Port changes from metis4 wrapper to metis5 wrapper

--- src/metis5_wrapper.f90
+++ src/metis5_wrapper.f90
@@ -11,6 +11,8 @@ module spral_metis_wrapper
    private
    public :: metis_order ! Calls metis on a symmetric matrix
 
+   integer, parameter :: long = C_LONG
+
    ! We use the C interface to METIS via the following interoperable interfaces
    interface
       subroutine METIS_SetDefaultOptions(options) &
@@ -53,13 +55,14 @@ module spral_metis_wrapper
    ! Constants for this package
    integer, parameter :: ERROR_ALLOC = -1
    integer, parameter :: ERROR_N_OOR = -2
+   integer, parameter :: ERROR_NE_OOR = -3
    integer, parameter :: ERROR_UNKNOWN = -999
 contains
 
 !
 ! Fortran wrapper around metis
 !
-subroutine metis_order(n,ptr,row,perm,invp,flag,stat)
+subroutine metis_order32(n,ptr,row,perm,invp,flag,stat)
    integer, intent(in) :: n ! Must hold the number of rows in A
    integer, intent(in) :: ptr(n+1) ! ptr(j) holds position in row of start of
       ! row indices for column j. ptr(n)+1 must equal the number of entries
@@ -110,7 +113,7 @@ subroutine metis_order(n,ptr,row,perm,invp,flag,stat)
    endif
 
    ! Expand matrix, dropping diagonal entries
-   call half_to_full_drop_diag(n, ptr, row, ptr2, row2)
+   call half_to_full_drop_diag32_32(n, ptr, row, ptr2, row2)
 
    ! Carry out ordering
    call METIS_SetDefaultOptions(metis_opts)
@@ -130,11 +133,92 @@ subroutine metis_order(n,ptr,row,perm,invp,flag,stat)
       print *, "Unknown metis error with code ", metis_flag
       flag = ERROR_UNKNOWN
    end select
-end subroutine metis_order
+end subroutine metis_order32
+
+!
+! Fortran wrapper around metis
+!
+subroutine metis_order64(n,ptr,row,perm,invp,flag,stat)
+   integer, intent(in) :: n ! Must hold the number of rows in A
+   integer(long), intent(in) :: ptr(n+1) ! ptr(j) holds position in row of
+      ! start of row indices for column j. ptr(n)+1 must equal the number of
+      ! entries stored + 1. Only the lower triangular entries are stored with
+      ! no duplicates or out-of-range entries
+   integer, intent(in) :: row(:) ! size at least ptr(n+1)-1
+   integer, intent(out) :: perm(n) ! Holds elimination order on output
+   integer, intent(out) :: invp(n) ! Holds inverse of elimination order on exit
+   integer, intent(out) :: flag ! Return value
+   integer, intent(out) :: stat ! Stat value on allocation failure
+
+   ! ---------------------------------------------
+   ! Local variables
+   ! ---------------------------------------------
+   integer, allocatable :: ptr2(:) ! copy of pointers which is later modified
+   integer, allocatable :: row2(:) ! copy of row indices
+   integer :: metis_opts(METIS_NOPTIONS) ! metis options array
+   integer(long) :: iwlen ! length of iw
+
+   integer :: metis_flag
+
+   ! Initialise flag and stat
+   flag = 0
+   stat = 0
+
+   !
+   ! Check that restrictions are adhered to
+   !
+   if (n.lt.1) then
+      flag = ERROR_N_OOR
+      return
+   endif
+
+   if (n.eq.1) then
+      ! Matrix is of order 1 so no need to call ordering subroutines
+      perm(1) = 1
+      return
+   endif
+
+   ! Set length of iw
+   iwlen = 2*ptr(n+1) - 2
+   if(iwlen.gt.huge(ptr2)) then
+      ! Can't accomodate this many entries with 32-bit interface to metis
+      flag = ERROR_NE_OOR
+      return
+   endif
+
+   ! Allocate arrays
+   allocate (ptr2(n+1),row2(iwlen),stat=stat)
+   if (stat.ne.0) then
+      flag = ERROR_ALLOC
+      return
+   endif
+
+   ! Expand matrix, dropping diagonal entries
+   call half_to_full_drop_diag64_32(n, ptr, row, ptr2, row2)
+
+   ! Carry out ordering
+   call METIS_SetDefaultOptions(metis_opts)
+   metis_opts(METIS_OPTION_NUMBERING) = 1 ! Fortran-style numbering
+   metis_flag = METIS_NodeND(n, ptr2, row2, C_NULL_PTR, metis_opts, invp, perm)
+   select case(metis_flag)
+   case(METIS_OK)
+      ! Everything OK, do nothing
+   case(METIS_ERROR_MEMORY)
+      ! Ran out of memory
+      flag = ERROR_ALLOC
+      stat = -99 ! As we don't know the real value, make up a distinctive one
+      return
+   case default
+      ! Something else went wrong, report as unknown error
+      ! (Should never encounter this code)
+      print *, "Unknown metis error with code ", metis_flag
+      flag = ERROR_UNKNOWN
+   end select
+end subroutine metis_order64
 
 ! Convert a matrix in half storage to one in full storage.
 ! Drops any diagonal entries.
-subroutine half_to_full_drop_diag(n, ptr, row, ptr2, row2)
+subroutine half_to_full_drop_diag32_32(n, ptr, row, ptr2, row2)
    integer, intent(in) :: n
    integer, dimension(n+1), intent(in) :: ptr
    integer, dimension(ptr(n+1)-1), intent(in) :: row
@@ -177,6 +261,55 @@ subroutine half_to_full_drop_diag(n, ptr, row, ptr2, row2)
       ptr2(j) = ptr2(j) + 1
    end do
 
-end subroutine half_to_full_drop_diag
+end subroutine half_to_full_drop_diag32_32
+
+! Convert a matrix in half storage to one in full storage.
+! Drops any diagonal entries.
+! 64-bit to 32-bit ptr version. User must ensure no oor entries prior to call.
+subroutine half_to_full_drop_diag64_32(n, ptr, row, ptr2, row2)
+   integer, intent(in) :: n
+   integer(long), dimension(n+1), intent(in) :: ptr
+   integer, dimension(ptr(n+1)-1), intent(in) :: row
+   integer, dimension(*), intent(out) :: ptr2
+   integer, dimension(*), intent(out) :: row2
+
+   integer :: i, j
+   integer(long) :: kk
+
+   ! Set ptr2(j) to hold no. nonzeros in column j
+   ptr2(1:n+1) = 0
+   do j = 1, n
+      do kk = ptr(j), ptr(j+1) - 1
+         i = row(kk)
+         if (j.ne.i) then
+            ptr2(i) = ptr2(i) + 1
+            ptr2(j) = ptr2(j) + 1
+         end if
+      end do
+   end do
+
+   ! Set ptr2(j) to point to where row indices will end in row2
+   do j = 2, n
+      ptr2(j) = ptr2(j-1) + ptr2(j)
+   end do
+   ptr2(n+1) = ptr2(n) + 1
+
+   ! Fill ptr2 and row2
+   do j = 1, n
+      do kk = ptr(j), ptr(j+1) - 1
+         i = row(kk)
+         if (j.ne.i) then
+            row2(ptr2(i)) = j
+            row2(ptr2(j)) = i
+            ptr2(i) = ptr2(i) - 1
+            ptr2(j) = ptr2(j) - 1
+         end if
+      end do
+   end do
+   do j = 1, n
+      ptr2(j) = ptr2(j) + 1
+   end do
+
+end subroutine half_to_full_drop_diag64_32
 
 end module spral_metis_wrapper
