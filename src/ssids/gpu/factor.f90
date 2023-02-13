0a7a857879d3c9d0eeb0e1e634bfa07631d29d79
Marcel Jacobse <44684927+mjacobse@users.noreply.github.com>
2023-02-13 14:48:39 +0100

    Fix maxfront info in analysis and GPU factorization
    maxfront should be the maximum size of all frontal matrices, i.e. the
    maximum number of rows across all nodes. This maximum was already formed
    correctly in the CPU factorization. But in analysis and GPU
    factorization, the maximum node size was computed instead. Fixes #110

--- src/ssids/gpu/factor.f90
+++ src/ssids/gpu/factor.f90
@@ -491,7 +491,7 @@ contains
           blkn = sptr(node+1) - sptr(node) + ndelay
           blkm = int(rptr(node+1) - rptr(node)) + ndelay
     
-          stats%maxfront = max(stats%maxfront, blkn)
+          stats%maxfront = max(stats%maxfront, blkm)
 
           ! Allocate memory for the local permutation (lperm).
           call smalloc(alloc, nodes(node)%perm, blkn+0_long, &
