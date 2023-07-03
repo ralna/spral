92f357bebdf7e1a330337430324194d1abdeb7b4
Jari <jfowkes@users.noreply.github.com>
2023-07-03 16:47:43 +0100

    Use PAGE_SIZE = 0MB in AppendAlloc

--- src/ssids/cpu/AppendAlloc.hxx
+++ src/ssids/cpu/AppendAlloc.hxx
@@ -65,7 +65,7 @@ private:
  * Deallocation is not supported.
  */
 class Pool {
-   const size_t PAGE_SIZE = 8*1024*1024; // 8MB
+   const size_t PAGE_SIZE = 0; // 0MB
 public:
    Pool(size_t initial_size)
    : top_page_(new Page(std::max(PAGE_SIZE, initial_size)))
